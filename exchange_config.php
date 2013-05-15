b8564e9f4684b044a3c9f4c7185161715432d98d
Antonino Di Bella <dibella.antonino@gmail.com>
2013-05-15 03:17:10 +0200

    Create exchange_config.php

---
+++ exchange_config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("_EXCHSERVER_","https://www.example.com/"); //EXCHANGE SERVER
+define("_EXCHMAIL_","user@example"); //EMAIL
+define("_EXCHUSER_","user"); //USERNAME
+define("_EXCHPASSWORD_","password"); //PASSWORD
+
+
+define("_CALFOLDER_","calendar/"); //CALENDAR FOLDER
+define("_MAILFOLDER_","inbox/"); //INBOX FOLDER
+define("_EXCHFOLDER_","Exchange/"); //EXCHANGE FOLDER
+
+?>
